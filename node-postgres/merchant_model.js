6af2de4fd40981904b4b5a2d47ffedea501b42bd
Zarubin Alexander <zarubin_14@list.ru>
2023-04-14 12:51:15 +0300

    Connect ReactApp + ExpressPostgres

---
+++ node-postgres/merchant_model.js
@@ -0,0 +1,52 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  host: 'localhost',
+  user: 'my_user',
+  database: 'my_database',
+  password: 'root',
+  port: 5000,
+});
+
+const getMerchants = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM merchants ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const createMerchant = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { name, email } = body
+
+    pool.query('INSERT INTO merchants (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new merchant has been added added: ${JSON.stringify(results.rows[0])}`)
+    })
+  })
+}
+
+const deleteMerchant = (merchantId) => {
+  return new Promise(function(resolve, reject) {
+    const id = parseInt(merchantId)
+
+    pool.query('DELETE FROM merchants WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        reject('error on pool query')
+      }
+      resolve(`Merchant deleted with ID: ${id}`)
+    })
+  })
+}
+
+module.exports = {
+  getMerchants,
+  createMerchant,
+  deleteMerchant
+}
